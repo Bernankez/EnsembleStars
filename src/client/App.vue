4ee9dcc05628270cf8164ae50ec60826055623a6
cole <bernankez@qq.com>
2022-06-26 00:56:36 +0800

    chore: update vite config

---
+++ src/client/App.vue
@@ -0,0 +1,6 @@
+<template>
+  Hello, World.
+  <ESButton />
+</template>
+
+<script setup lang="ts"></script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "es-ui",
+      fileName: module => `es-ui.${module}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
