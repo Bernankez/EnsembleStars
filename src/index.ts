d8fc5b1f3df80c7d1e88fa0f6db56400a194ebac
cole <bernankez@qq.com>
2022-06-26 19:12:24 +0800

    chore: add sass

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { App } from "vue";
 import * as components from "./components";
-import "./styles/index.css";
+import "./styles/index.scss";
 import { forIn } from "lodash-es";
 
 const createES = () => ({
