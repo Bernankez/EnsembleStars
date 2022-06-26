934ebb477544c24a65733a21f09999153fce55ef
cole <bernankez@qq.com>
2022-06-26 11:04:19 +0800

    chore: type declaration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      outputDir: "dist",
+      staticImport: true,
+      insertTypesEntry: true,
+    }),
+  ],
   test: {
     environment: "happy-dom",
   },
