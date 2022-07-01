4e3eb102ffe3f671fb1b9ac74d0e178d52a210c5
cole <bernankez@qq.com>
2022-07-02 00:20:39 +0800

    fix: build lib name

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "es-ui",
+      name: "es",
       fileName: module => `es-ui.${module}.js`,
     },
     rollupOptions: {
